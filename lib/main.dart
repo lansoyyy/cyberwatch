f981145ae69ed803d0b7e44382cb7986403ab82b
Lans12345 <olanalans12345@gmail.com>
2023-07-03 08:43:19 +0800

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:cyberwatch/screens/auth/login_screen.dart';
 import 'package:cyberwatch/utils/colors.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyCUSrbTWEnhyMM3d5VRDnMQzOO95L_nG-8",
+          appId: "1:177303697074:web:d45849f1c197fd0421bbab",
+          messagingSenderId: "177303697074",
+          projectId: "cyberwatch-dfd11",
+          storageBucket: "cyberwatch-dfd11.appspot.com"));
   runApp(const MyApp());
 }
 
